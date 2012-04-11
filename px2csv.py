675ae04cd826520ddfed1080636c6ab8d015df64
Donal Boyle <donalboyle@gmail.com>
2012-04-10 20:42:54 -0400

    cleaned up pyc files

--- px2csv.py
+++ px2csv.py
@@ -2,21 +2,20 @@
  Converts px-axis formatted files to csv.
 """
 
-class PxConverter:
-    def verify_cx_text(self, cx_text):
-        """
-        Checks that the provided text is cc-axis formated.
-        Returns true if it is cx text, otherwise false.
-        TODO The pc-axis format probably has several other things that should be checked.
-        """
-        if not isinstance(cx_text, basestring):
-            return False
-        return cx_text.startswith('AXIS-VERSION="2000"')
+def verify_cx_text(cx_text):
+    """
+    Checks that the provided text is cc-axis formated.
+    Returns true if it is cx text, otherwise false.
+    TODO The pc-axis format probably has several other things that should be checked.
+    """
+    if not isinstance(cx_text, basestring):
+        return False
+    return cx_text.startswith('AXIS-VERSION="2000"')
         
 
-    def px_to_csv(self, px):
-        """
-        Converts the provided text in px format to csv text.
-        """
-        return px
+def px_to_csv(self, px):
+    """
+    Converts the provided text in px format to csv text.
+    """
+    return px
 
--- px2csv_test.py
+++ px2csv_test.py
@@ -9,17 +9,16 @@ import px2csv
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
-        self.px2csv = px2csv.PxConverter()
         self.test_px = open("test_data/holdings.px")
 
     def test_verify_cx_text(self):
-        self.assertFalse(self.px2csv.verify_cx_text(123));
+        self.assertFalse(px2csv.verify_cx_text(123));
         # make sure true is returned for valid text
         px_text = self.test_px.read()
-        self.assertTrue(self.px2csv.verify_cx_text(px_text));
+        self.assertTrue(px2csv.verify_cx_text(px_text));
         
         # false for invalid text
-        self.assertFalse(self.px2csv.verify_cx_text("Gobilty gook"));
+        self.assertFalse(px2csv.verify_cx_text("Gobilty gook"));
 
 #    def test_px_to_csv(self):
 
